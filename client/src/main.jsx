704a132864c657dd947e6a6db6e2cf7c2316266e
unknown <cristiangc92@gmail.com>
2023-06-17 00:08:02 -0300

    Configuraciones para el deploy!

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import { store } from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 
-axios.defaults.baseURL = "";
+axios.defaults.baseURL = "https://pokemon-spa.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
