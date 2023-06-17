91821f14ad7a88942b764b66055b75e7504a6d14
unknown <cristiangc92@gmail.com>
2023-06-17 00:13:17 -0300

    Configuraciones para el deploy!

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
+import axios from "axios";
 
 axios.defaults.baseURL = "https://pokemon-spa.onrender.com";
 
